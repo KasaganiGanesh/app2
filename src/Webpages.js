b124fdf346e108794fa82c6005737bfa191b362f
KasaganiGanesh <ganeshkasagani@lyrostech.com>
2024-03-11 09:39:19 +0530

    ...

--- src/Webpages.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Webpages() {
-  return (
-    <div>
-        <a href="https://kasaganiganesh.github.io/myapp1/">project 1</a>
-    </div>
-  )
-}
